0e34e2c81d0f69e646797917866530932655c74c
pica69co <oscarwarrieta@gmail.com>
2025-02-08 10:37:06 -0500

    initial commit basic layout

---
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { UserCircleIcon } from "lucide-react";
+import React from "react";
+
+export const AuthButton = () => {
+  // TODO: Implement auth logic with different states
+  return (
+    <Button
+      variant="outline"
+      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/2 rounded-full shadow-none"
+    >
+      <UserCircleIcon className="" />
+      Sign in
+    </Button>
+  );
+};
