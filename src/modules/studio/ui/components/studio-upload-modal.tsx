7482d06c5af1b1359c2d4a7dd61710b0d1ecd991
pica69co <oscarwarrieta@gmail.com>
2025-02-13 20:45:56 -0500

    create video responsive dialog/create upload vide modal

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button";
 import { Loader2Icon, PlusIcon } from "lucide-react";
 import { trpc } from "@/trpc/client";
 import { toast } from "sonner";
+import { ResponsiveModal } from "@/components/responsive-modal";
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModal = () => {
   const utils = trpc.useUtils();
@@ -18,17 +20,30 @@ export const StudioUploadModal = () => {
   });
 
   return (
-    <Button
-      variant="secondary"
-      onClick={() => create.mutate()}
-      disabled={create.isPending}
-    >
-      {create.isPending ? (
-        <Loader2Icon className="animate-spin" />
-      ) : (
-        <PlusIcon />
-      )}
-      {create.isPending ? "Wait" : "Create"}
-    </Button>
+    <>
+      <ResponsiveModal
+        title="Upload Video"
+        open={!!create.data?.url}
+        onOpenChange={() => create.reset()}
+      >
+        {create.data?.url ? (
+          <StudioUploader endpoint={create.data?.url} onSuccess={() => {}} />
+        ) : (
+          <Loader2Icon />
+        )}
+      </ResponsiveModal>
+      <Button
+        variant="secondary"
+        onClick={() => create.mutate()}
+        disabled={create.isPending}
+      >
+        {create.isPending ? (
+          <Loader2Icon className="animate-spin" />
+        ) : (
+          <PlusIcon />
+        )}
+        {create.isPending ? "Wait" : "Create"}
+      </Button>
+    </>
   );
 };
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,19 +1,31 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
+
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+      },
+      cors_origin: "*", // TODO: In Production, set to url
+    });
+
     const [video] = await db
       .insert(videos)
       .values({
         userId,
         title: "Untitled",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
       })
       // returning() is required to return the inserted row
       .returning();
 
-    return { video: video };
+    return { video: video, url: upload.url };
   }),
 });
