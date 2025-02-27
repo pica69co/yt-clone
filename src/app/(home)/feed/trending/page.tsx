37a4424b3356bcf80d22da8cf853ec3abccbe9bd
pica69co <oscarwarrieta@gmail.com>
2025-02-27 17:15:45 -0500

    add trending section: implement trending videos view with infinite scrolling and error handling

---
+++ src/app/(home)/feed/trending/page.tsx
@@ -0,0 +1,22 @@
+import { HydrateClient, trpc } from "@/trpc/server";
+import { DEFAULT_LIMIT } from "@/constants";
+
+import TrendingView from "@/modules/home/ui/views/trending-view";
+
+export const dynamic = "force-dynamic";
+
+const Page = async () => {
+  void trpc.videos.getManyTrending.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <div>
+      <HydrateClient>
+        <TrendingView />
+      </HydrateClient>
+    </div>
+  );
+};
+
+export default Page;
